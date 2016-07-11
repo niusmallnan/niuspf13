442bac844474622bd169f6069e7b4c8b575a554c
niusmallnan <zhangzb@neunn.com>
2016-07-11 08:41:28 +0000

    update conf

--- install.sh
+++ install.sh
@@ -8,13 +8,12 @@ apt-get update && apt-get install -y python-dev exuberant-ctags vim-nox
 
 # for python pip
 mkdir ~/.pip && cp pip.conf ~/.pip/
-wget https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py
-python /tmp/get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm -f get-pip.py
 pip install virtualenv
 
 # for spf13 vim
 cp .vimrc.before.local  ~/
 cp .vimrc.bundles.local ~/
 cp .vimrc.local ~/
-curl http://j.mp/spf13-vim3 -L -o - | sh
-
+# http://j.mp/spf13-vim3
+curl https://raw.githubusercontent.com/spf13/spf13-vim/3.0/bootstrap.sh -L -o - | sh
