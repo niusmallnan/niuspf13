d023cb3f33cb8c4396a8094921b1e0e3e7be61ec
niusmallnan <niusmallnan@gmail.com>
2017-10-03 15:04:58 +0800

    Update go_tools

--- install_gotools.sh
+++ install_gotools.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-TMP_DIR='/tmp'
-GO_SRC_DIR='$GOPATH/src'
+TMP_DIR="/tmp"
+GO_SRC_DIR="$GOPATH/src"
+echo $GO_SRC_DIR
 
 cd $TMP_DIR
 git clone https://github.com/golang/tools.git
 
 cd $GO_SRC_DIR
-mkdir -p golang.org/x
+mkdir -p golang.org/x/tools && rm -rf golang.org/x/tools/
 mv $TMP_DIR/tools golang.org/x/
 
 go get golang.org/x/tools/cmd/goimports
